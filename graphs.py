6d84b954997b81c82446266795ad1c63baced7e0
paolovasquezg <paolo.vasquez@utec.edu.pe>
2022-11-20 19:24:07 -0500

    graficas realizadas

--- graphs.py
+++ graphs.py
@@ -4,22 +4,22 @@ import matplotlib.pyplot as plt
 
 x = []
 y = []
-with open('C1_indice.txt') as archivo: #Cambiar el nombre del archivo correspondiente
+with open('C3_indice.txt') as archivo: #Cambiar el nombre del archivo correspondiente
     for linea in archivo:
         punto = [int(i) for i in linea.split(',')]
         x.append(punto[0])
         y.append(punto[1])
-plt.plot(x, y, "o--", label="Indice")
+plt.plot(x, y, "o--", label="Índice")
 plt.legend(loc="best" )
 
 x = []
 y = []
-with open('sin_indice.txt') as archivo:  # Cambiar el nombre del archivo correspondiente
+with open('C3_sin_indice.txt') as archivo:  # Cambiar el nombre del archivo correspondiente
     for linea in archivo:
         punto = [int(i) for i in linea.split(',')]
         x.append(punto[0])
         y.append(punto[1])
-plt.plot(x, y, "o--", label="C1_sin indice")
+plt.plot(x, y, "o--", label="Sin índice")
 plt.legend(loc="best" )
 
 
@@ -50,8 +50,8 @@ plt.yscale("log")
 # plt.ylim(0, 123000)
 plt.ylabel("Tiempo en ms")
 plt.xlabel("Cantidad de datos")
-plt.title("Consulta 1: Sin indice vs indice")
-plt.savefig("Consulta1.jpg")
+plt.title("Consulta 3: Sin índice vs índice")
+plt.savefig("Consulta3.jpg")
 plt.show()
 
 #Cambiar los respectivos nombre de acuerdo a la conveniencia
